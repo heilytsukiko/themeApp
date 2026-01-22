4dfcb5ce4fa919e0d5c826f44ef5f275f15b5abb
dor <heilytsukiko@gmail.com>
2026-01-23 00:15:56 +0500

    fix: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/themeApp/",
+   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
 })
